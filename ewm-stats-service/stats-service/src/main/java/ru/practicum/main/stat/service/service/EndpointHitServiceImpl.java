64a25a2981081a398567006dcbe197d596f85a6e
melentyevSergey <melentyev.sergey@yandex.ru>
2024-01-15 23:09:39 +0300

    Stage 2. Main service.

--- ewm-stats-service/stats-service/src/main/java/ru/practicum/main/stat/service/service/EndpointHitServiceImpl.java
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/main/stat/service/service/EndpointHitServiceImpl.java
@@ -32,6 +32,7 @@ public class EndpointHitServiceImpl implements EndpointHitService {
     }
 
     @Override
+    @Transactional
     public List<ViewStats> stats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
         List<ViewStats> list = new ArrayList<>();
 
